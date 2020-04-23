31159ef063c1d39d1155f7ecccffd44091f0564d
tamasmeszaros <meszaros.q@gmail.com>
2020-04-23 10:22:11 +0200

    Update dependencies to include wxWidgets 3.1.3 cmake based build
    Fix on win

--- deps/deps-unix-common.cmake
+++ deps/deps-unix-common.cmake
@@ -9,7 +9,12 @@ endif ()
 
 find_package(ZLIB QUIET)
 if (NOT ZLIB_FOUND)
-    include(ZLIB/ZLIB.cmake)
+    message(WARNING "No ZLIB dev package found in system, building static library. You should install the system package.")
+endif ()
+
+find_package(EXPAT QUIET)
+if (NOT EXPAT_FOUND)
+    message(WARNING "No EXPAT dev package found in system, building static library. Consider installing the system package.")
 endif ()
 
 ExternalProject_Add(dep_tbb
